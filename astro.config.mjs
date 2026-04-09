6ef9fe2b970c2f76cfe8e6ce43b49e45bc06dd9a
anig <alexisnahuelidoyaga@gmail.com>
2026-04-09 15:31:12 -0300

    commit para añadir adaptador de versel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
 
-import vercel from '@astrojs/vercel/serverless'; // Importación recomendada para SSR
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
 
-  // Eliminamos la línea duplicada que decía adapter: 'vercel()' como string
   adapter: vercel({
     webAnalytics: {
       enabled: true,
