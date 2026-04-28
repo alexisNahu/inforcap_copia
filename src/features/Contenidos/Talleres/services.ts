9eedb3a70bfd393d14125e8a5bf98c385ef84a18
anig <alexisnahuelidoyaga@gmail.com>
2026-04-28 13:39:02 -0300

    error arreglado

--- src/features/Contenidos/Talleres/services.ts
+++ src/features/Contenidos/Talleres/services.ts
@@ -1,26 +1,14 @@
-import { apiClient } from "@/core/api/config.ts";
-import { API_ENDPOINTS } from "@/core/api";
-import type { Taller } from "./models.ts";
+import {apiClient} from "@/core/api/config.ts";
+import {API_ENDPOINTS} from "@/core/api";
+import type {Taller} from "./models.ts";
 
-function mapToTaller(data: any): Taller {
-    const process = (val: any) => ({
-        ...val,
-        slug: val.slug?.replace(/[\.;]/g, '') ?? '',
-        aprenderas_html: val.aprenderas_html ? val.aprenderas_html.split('-') : [],
-        tipo: 'Taller' as const
-    });
-
-    return Array.isArray(data) ? data.map(process) : process(data);
-}
 
 export const TallerService = {
     async getAll(): Promise<Taller[]> {
-        const data = await apiClient<any[]>(API_ENDPOINTS.talleres);
-        return mapToTaller(data) as unknown as Taller[];
+        return await apiClient<any[]>(API_ENDPOINTS.talleres);
     },
 
     async getByIdOrSlug(param: number | string): Promise<Taller> {
-        const data = await apiClient<any>(`${API_ENDPOINTS.talleres}/${param}`);
-        return mapToTaller(data) as Taller;
+        return await apiClient<any>(`${API_ENDPOINTS.talleres}/${param}`);
     }
 };
