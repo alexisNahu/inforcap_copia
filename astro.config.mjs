6d75edf72b87e70456d82bce40f1ecaf3678d0b6
anig <alexisnahuelidoyaga@gmail.com>
2026-05-13 14:10:38 -0300

    haciendo el cambio a produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import path from 'path';
 import vercel from '@astrojs/vercel';
 
-// Verificamos si estamos en Vercel o si queremos forzar el modo servidor
 const isVercel = process.env.VERCEL === '1';
 
 export default defineConfig({
