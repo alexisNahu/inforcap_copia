0d26ec8b833da1bc67d08c466397e1a5f45a01fc
anig <alexisnahuelidoyaga@gmail.com>
2026-06-04 09:17:19 -0300

    intentando configurar el ercel para traer automatico los cambios

---
+++ src/features/Talleres/models.ts
@@ -0,0 +1,14 @@
+export interface Taller {
+    id: number;
+    slug: string;
+    titulo: string;
+    imagen_portada: string;
+    mes: string;
+    dias: string;
+    fecha_inicio: string;
+    horario: string;
+    precio: string;
+    estado: number;
+    duracion?: string;
+    aprenderas_html?: string[];
+}
